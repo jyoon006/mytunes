200402f15c14f5589fb74b7178e49bd30b70141c
Jisoo Yoon <jyoon006@gmail.com>
2016-01-28 20:30:08 -0500

    able to incremnt to playCount each time song is played

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,8 +1,15 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
 
+  defaults: {
+    'playCount': 0
+  },
+
   play: function(){
     // Triggering an event here will also trigger the event on the collection
+    //console.log(song.set("playCount", song.get('playCount')));
+    this.set('playCount', this.get('playCount')+1)
+    console.log(this.get('playCount'));
     this.trigger('play', this);
   },
   enqueue: function() {
